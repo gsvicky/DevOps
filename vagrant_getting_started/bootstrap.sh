b929dcd9b80817d5f1a7740f5be1198e8f0ad9ba
VictorG <gsvicky@gmail.com>
2013-05-23 19:04:53 -0700

    Changed for git

--- vagrant_getting_started/bootstrap.sh
+++ vagrant_getting_started/bootstrap.sh
@@ -1,6 +1,6 @@
 # Custom settings
 ##############################################################################
-echo "export PS1='---\[\033[02;32m\]\u@\H:\[\033[02;36m\]\w\$\[\033[00m\] '" >> ~/.bash_profile
+echo "export PS1='\[\033[02;32m\]\u@\H:\[\033[02;36m\]\w\$\[\033[00m\] '" >> ~/.bash_profile
 cd /etc/yum.repos.d/
 mkdir /opt/temp/
 
@@ -18,28 +18,30 @@ rpm -Uvhe  vagrant_1.2.2_x86_64.rpm
 # Install Git & pull code
 ##############################################################################
 yum -y install git
+git config --global user.name "Victor Gajendran"
+git config --global user.email "gsvicky@gmail.com"
+
 cd /usr/local/src/
 git clone https://github.com/gsvicky/DevOps.git
 
 # Install & configure Chef-client
 ##############################################################################
 true && curl -L https://www.opscode.com/chef/install.sh | bash
-git clone git://github.com/opscode/chef-repo /usr/local/src/DevOps/chef_getting_started/chef-repo
-mkdir -p /usr/local/src/DevOps/chef_getting_started/chef-repo/.chef
-cp /vagrant/knife.rb /usr/local/src/DevOps/chef_getting_started/chef-repo/.chef
-cp /vagrant/adeptize-validator.pem /usr/local/src/DevOps/chef_getting_started/chef-repo/.chef
-cp /vagrant/gsvicky.pem /usr/local/src/DevOps/chef_getting_started/chef-repo/.chef
 
-#Add .chef to the .gitignore file to prevent uploading the contents of the .chef folder to github.
-##############################################################################
-echo '.chef' >> /usr/local/src/DevOps/chef_getting_started/chef-repo/.gitignore
+mkdir -p /usr/local/src/DevOps/configManagement/chef-repo/.chef
+cp /vagrant/knife.rb /usr/local/src/DevOps/configManagement/chef-repo/.chef
+cp /vagrant/adeptize-validator.pem /usr/local/src/DevOps/configManagement/chef-repo/.chef
+cp /vagrant/gsvicky.pem /usr/local/src/DevOps/configManagement/chef-repo/.chef
 
 # For convenience, create a link to the src folder in home
 ##############################################################################
-ln -s /usr/local/src/DevOps/chef_getting_started/chef-repo ~/devfolder
+ln -s /usr/local/src/DevOps/configManagement/chef-repo ~/chefdir
 
 # Cleanup
 ##############################################################################
 rm -rf /opt/temp/
 
 
+##############################################################################
+# WHEN DONE SETUP GIT SSH KEY https://help.github.com/articles/generating-ssh-keys
+##############################################################################
