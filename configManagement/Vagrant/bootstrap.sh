3b781e5c34d5888a48b9476654b06eb537214cc2
VictorG <gsvicky@gmail.com>
2013-05-25 22:58:14 -0700

    Created chef node

---
+++ configManagement/Vagrant/bootstrap.sh
@@ -0,0 +1,16 @@
+# Custom settings
+##############################################################################
+echo "export PS1='\[\033[02;34m\]\u@\H:\[\033[02;37m\]\w\$\[\033[00m\] '" >> ~/.bash_profile
+
+
+
+# Install & configure Chef-client
+##############################################################################
+#true && curl -L https://www.opscode.com/chef/install.sh | bash
+
+#mkdir -p /usr/local/src/DevOps/configManagement/chef-repo/.chef
+#cp /vagrant/knife.rb /usr/local/src/DevOps/configManagement/chef-repo/.chef
+#cp /vagrant/adeptize-validator.pem /usr/local/src/DevOps/configManagement/chef-repo/.chef
+#cp /vagrant/gsvicky.pem /usr/local/src/DevOps/configManagement/chef-repo/.chef
+
+
